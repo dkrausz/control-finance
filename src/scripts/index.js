99bccb9f40d04778409917ee9a47ac8125c7d06e
Danilo Krausz <danilo.krausz@gmail.com>
2023-10-30 21:21:18 +0100

    iniciada a programacao dos cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,42 @@
 /* Desenvolva sua lógica aqui */
+
+function creatItem(item){
+const title = document.createElement("h3");
+const divButtons = document.createElement("div");
+const category = document.createElement("span");
+const delButton = document.createElement("button");
+const card = document.createElement("li");
+
+title.classList.add("item__price");
+divButtons.classList.add("item__buttons");
+category.classList.add("item__category");
+delButton.classList.add("item__del-button");
+card.classList.add("item__container");
+
+title.innerText=item.value;
+category.innerText=valuesCategory[item.categoryID];
+
+divButtons.append(category,delButton);
+card.append(title,divButtons);
+
+delButton.addEventListener("click",(event)=>{
+const index = insertedValues.indexOf(item);
+insertedValues.splice(index,1);
+    console.log(index);
+    renderCards(insertedValues);
+})
+
+return card;
+}
+
+function renderCards(list){
+    
+    const itemList = document.querySelector(".list__itens");
+    itemList.innerHTML= ' ';
+    for(item of list){
+        itemList.appendChild(creatItem(item));
+    }
+
+}
+
+renderCards(insertedValues);
